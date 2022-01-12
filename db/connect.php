c521419731667068a223d41d5ac8a6e347ff7226
KAWSER04 <37189342+KAWSER04@users.noreply.github.com>
2022-01-13 00:02:36 +0600

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -3,7 +3,7 @@ function Connect()
 {
     $server_name = "sql6.freesqldatabase.com";
     $username = "sql6465229";
-    $password = "";
+    $password = "KEUf6ye7S9";
     $db_name = "sql6465229";
     try {
         $conn = new PDO("mysql:host=$server_name;dbname=$db_name", $username, $password);
