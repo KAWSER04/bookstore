4641e8ded973f697de953d743a20f3bcc6fb17de
KAWSER04 <37189342+KAWSER04@users.noreply.github.com>
2022-01-12 23:56:54 +0600

    Add files via upload

---
+++ result.php
@@ -0,0 +1,69 @@
+<?php require_once 'functions.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Search Result</title>
+</head>
+
+<body>
+
+<?php
+	if (isset($_POST['id'])){
+		$id = $_POST['id'];
+	}
+
+	if (file_exists('books.json')) {
+       	$booksJson = file_get_contents('books.json');
+				$books = json_decode($booksJson, true);
+  } else {
+      $books = array();
+  }
+
+  $ck = false;
+  foreach ($books as $book) {
+    if ($book['id'] == $id) {
+    	$result = $book;
+    	$ck = true;
+    }
+  }
+
+  if($ck == false)
+  	header('Location: 404.php');
+?>
+
+<h1 align = "center"> Showing Search Result </h1>
+<table align="center" border="2">
+	<tr>
+		<th> Id </th>
+		<th> Title </th>
+		<th> Author </th>
+		<th> Pages </th>
+		<th> Available </th>
+		<th> Isbn </th>
+	</tr>
+	<tr>
+		<td>
+			<?php echo $result['id']; ?>
+		</td>
+		<td>
+			<?php echo $result['title']; ?>
+		</td>
+		<td>
+			<?php echo $result['author']; ?>
+		</td>
+		<td>
+			<?php echo $result['pages']; ?>
+		</td>
+		<td>
+			<?php echo formateAvailable($result); ?>
+		</td>
+		<td>
+			<?php echo $result['isbn']; ?>
+		</td>
+	</tr>
+</table>
+</div>
+</body>
+</html>
